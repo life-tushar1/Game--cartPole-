51d3206a4d43a3fabe54d8cf0e56fa9285f5539f
life-tushar1 <life.tushar@gmail.com>
2017-12-29 02:01:00 +0530

    achieved the passing score of 195 ..this model scored 200

--- training.py
+++ training.py
@@ -66,11 +66,11 @@ def initial_population():
 def neural_network_model(input_size):
     network=input_data(shape=[None,input_size,1],name='input')
 
+    network=fully_connected(network,32,activation='relu')
+    network=dropout(network,0.8)
     network=fully_connected(network,128,activation='relu')
     network=dropout(network,0.8)
     network=fully_connected(network,256,activation='relu')
-    network=dropout(network,0.8)
-    network=fully_connected(network,512,activation='relu')
     network=dropout(network,0.8)
     network=fully_connected(network,256,activation='relu')
     network=dropout(network,0.8)
@@ -86,10 +86,44 @@ def train_model(training_data,model=False):
     y=[i[1] for i in training_data]
     if not model:
         model=neural_network_model(input_size=len(X[0]))
-    model.fit({'input':X},{'targets':y},n_epoch=5,snapshot_step=500,show_metric=True,run_id='gaming1')
+    model.fit({'input':X},{'targets':y},n_epoch=1,snapshot_step=500,show_metric=True,run_id='gaming1')
     return model
 
 training_data=initial_population()
 model=train_model(training_data)
 
+
+scores=[]
+choices=[]
+
+
+for each_game in range(10):
+    score=0
+    game_memory=[]
+    prev_obs=[]
+    env.reset()
+    for _ in range(goal_steps):
+        env.render()
+        if len(prev_obs)==0:
+            action=random.randrange(0,2)
+        else:
+            action=np.argmax(model.predict(prev_obs.reshape(-1,len(prev_obs),1))[0])
+        choices.append(action)
+
+        new_observation,reward,done,info=env.step(action)
+        prev_obs=new_observation
+        game_memory.append([new_observation,action])
+        score+=reward
+        if done:
+            break
+
+    scores.append(score)
+print('avg scores',sum(scores)/len(scores))
+print('Choice 1: {}, Choice 0 {}'.format((choices.count(1)/len(choices)),(choices.count(0)/len(choices))))
+'''print 'choice 1='+str(choices.count(1)/len(choices))
+print 'choice 0='+str(choices.count(0)/len(choices))'''
+print choices.count(0)
+print len(choices)
+print choices.count(1)
+model.save('passed.model')
 #initial_population()
